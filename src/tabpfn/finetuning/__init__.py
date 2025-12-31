c62f32d3f1a4632211134b9c7df6ff7b5f820754
Benjamin Jaeger <jaeger.benjamin7@gmail.com>
2025-12-31 15:07:16 +0100

    add abstract base class for finetuning wrappers

--- src/tabpfn/finetuning/__init__.py
+++ src/tabpfn/finetuning/__init__.py
@@ -1,9 +1,12 @@
 """Single-dataset fine-tuning wrappers for TabPFN models."""
 
+from tabpfn.finetuning.finetuned_base import EvalResult, FinetunedTabPFNBase
 from tabpfn.finetuning.finetuned_classifier import FinetunedTabPFNClassifier
 from tabpfn.finetuning.finetuned_regressor import FinetunedTabPFNRegressor
 
 __all__ = [
+    "EvalResult",
+    "FinetunedTabPFNBase",
     "FinetunedTabPFNClassifier",
     "FinetunedTabPFNRegressor",
 ]
