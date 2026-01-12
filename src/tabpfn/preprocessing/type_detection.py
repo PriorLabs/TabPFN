a540824e82b63d60154c237e180763092f98e59a
alanarazi7 <alanarazi7@gmail.com>
2026-01-12 10:28:40 +0200

    First implementation of DatasetView

--- src/tabpfn/preprocessing/type_detection.py
+++ src/tabpfn/preprocessing/type_detection.py
@@ -2,15 +2,22 @@
 
 from __future__ import annotations
 
+from collections import defaultdict
 from collections.abc import Sequence
+from dataclasses import dataclass
+from enum import Enum
 from typing import TYPE_CHECKING
 
-import pandas as pd
+from pandas import Series
+
+
+from tabpfn.constants import XType
 
 if TYPE_CHECKING:
     import numpy as np
 
 
+# TODO: 'infer_categorical_features' should be deprecated, to use the new 'detect_feature_types'.
 def infer_categorical_features(
     X: np.ndarray,
     *,
@@ -51,7 +58,7 @@ def infer_categorical_features(
     for ix, col in enumerate(X.T):
         # Calculate total distinct values once, treating NaN as a category.
         try:
-            s = pd.Series(col)
+            s = Series(col)
             # counts NaN/None as a category
             num_distinct = s.nunique(dropna=False)
         except TypeError as e:
@@ -70,3 +77,103 @@ def infer_categorical_features(
             indices.append(ix)
 
     return indices
+
+
+@dataclass(frozen=True)
+class DatasetView:
+    x_num: XType
+    x_cat: XType
+    x_text: XType
+
+
+class FeatureType(str, Enum):
+    NUMERICAL = "numerical"
+    CATEGORICAL = "categorical"
+    TEXTUAL = "textual"
+    CONSTANT = "constant"
+    MISSING = "missing"
+
+
+def detect_feature_types(X: np.ndarray,     
+    *,
+    min_samples_for_inference: int,
+    max_unique_for_category: int,
+    min_unique_for_numerical: int,
+    reported_categorical_indices: Sequence[int] | None = None,) -> DatasetView:
+    """Infer the categorical features from the given data.
+
+    !!! note
+
+        This function may infer particular columns to not be categorical
+        as defined by what suits the model predictions and it's pre-training.
+
+    Args:
+        X: The data to infer the categorical features from.
+        provided: Any user provided indices of what is considered categorical.
+        min_samples_for_inference:
+            The minimum number of samples required
+            for automatic inference of features which were not provided
+            as categorical.
+        max_unique_for_category:
+            The maximum number of unique values for a
+            feature to be considered categorical.
+        min_unique_for_numerical:
+            The minimum number of unique values for a
+            feature to be considered numerical.
+
+    Returns:
+        A DatasetView object with the features types.
+    """
+    type2idx = defaultdict(list)
+    large_enough_x_to_infer = X.shape[0] > min_samples_for_inference
+    for idx, col in enumerate(X.T):
+        reported_categorical = idx in (reported_categorical_indices or ())
+        feat_type = _detect_feature_type(X[:, col], reported_categorical=reported_categorical, max_unique_for_category=max_unique_for_category, min_unique_for_numerical=min_unique_for_numerical, large_enough_x_to_infer_categorical=large_enough_x_to_infer)
+        type2idx[feat_type].append(idx)
+    x_num = X[:, type2idx[FeatureType.NUMERICAL]]
+    x_cat = X[:, type2idx[FeatureType.CATEGORICAL]]
+    x_text = X[:, type2idx[FeatureType.TEXTUAL]]
+    return DatasetView(x_num, x_cat, x_text)
+
+
+
+def _detect_feature_type(col: np.ndarray, reported_categorical: bool, max_unique_for_category: int, min_unique_for_numerical: int, large_enough_x_to_infer_categorical: bool) -> FeatureType:
+    # Calculate total distinct values once, treating NaN as a category.
+
+    # TODO: detect whether it's a constant feature
+    # TODO: first, check if the feature is numeric or not
+
+    s = _array_to_series(col)
+    if _detect_categorical(s, reported_categorical, max_unique_for_category, min_unique_for_numerical, large_enough_x_to_infer_categorical):
+        return FeatureType.CATEGORICAL
+    return FeatureType.NUMERICAL
+
+
+
+def _array_to_series(col: np.ndarray) -> Series:
+    # TODO (1): the last part here looks like something that should be part of the validation.
+    # TODO (2): what is the point of casting it to a series? wouldn't numpy have built in functions for this?
+    try:
+        return Series(col)
+    except TypeError as e:
+        # e.g. "unhashable type: 'dict'" when object arrays contain dicts
+        raise TypeError(
+            "argument must be a string or a number"
+            "(columns must only contain strings or numbers)"
+        ) from e
+
+def _detect_categorical(s: Series, reported_categorical: int, max_unique_for_category: int, min_unique_for_numerical: int, large_enough_x_to_infer_categorical: bool) -> bool:
+    """Detecting if a numerical feature is categorical depending on heuristics:
+    - Feature reported to be categorical are treated as such, as long as they aren't too-highly cardinal.
+    - For non-reported numerical ones, we infer them as such if they are sufficiently low-cardinal.
+    """
+    num_distinct = s.nunique(dropna=False)
+    if reported_categorical:
+        if num_distinct <= max_unique_for_category:
+            return True
+    elif (
+        large_enough_x_to_infer_categorical
+        and num_distinct < min_unique_for_numerical
+    ):
+        return True
+    return False
