148a7d821613663da28c1729f3a69de74c19f2ca
Brendan Roof <brendan@priorlabs.ai>
2026-02-24 22:20:13 +0100

    allow browser

--- tests/quick_test.py
+++ tests/quick_test.py
@@ -20,45 +20,43 @@ logging.basicConfig(level=logging.INFO)
 
 
 if __name__ == "__main__":
-    # Patch webbrowser.open to prevent browser login
-    with patch("webbrowser.open", return_value=False):
-        use_server = True
-        # use_server = False
+    use_server = True
+    # use_server = False
 
-        X, y = load_breast_cancer(return_X_y=True)
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.33, random_state=42
-        )
+    X, y = load_breast_cancer(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.33, random_state=42
+    )
 
-        tabpfn = TabPFNClassifier.create_default_for_version(
-            ModelVersion.V2_5, n_estimators=3
-        )
-        # print("checking estimator", check_estimator(tabpfn))
-        tabpfn.fit(X_train[:99], y_train[:99])
-        print("predicting")
-        print(tabpfn.predict(X_test))
-        print("predicting_proba")
-        print(tabpfn.predict_proba(X_test))
+    tabpfn = TabPFNClassifier.create_default_for_version(
+        ModelVersion.V2_5, n_estimators=3
+    )
+    # print("checking estimator", check_estimator(tabpfn))
+    tabpfn.fit(X_train[:99], y_train[:99])
+    print("predicting")
+    print(tabpfn.predict(X_test))
+    print("predicting_proba")
+    print(tabpfn.predict_proba(X_test))
 
-        # can be slow if you have a lot of data
-        # print(UserDataClient.get_data_summary())
+    # can be slow if you have a lot of data
+    # print(UserDataClient.get_data_summary())
 
-        X, y = load_diabetes(return_X_y=True)
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.33, random_state=42
-        )
+    X, y = load_diabetes(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.33, random_state=42
+    )
 
-        tabpfn = TabPFNRegressor.create_default_for_version(
-            ModelVersion.V2_5, n_estimators=3
-        )
-        # print("checking estimator", check_estimator(tabpfn))
-        tabpfn.fit(X_train[:99], y_train[:99])
-        print("predicting reg")
-        print(tabpfn.predict(X_test, output_type="mean"))
+    tabpfn = TabPFNRegressor.create_default_for_version(
+        ModelVersion.V2_5, n_estimators=3
+    )
+    # print("checking estimator", check_estimator(tabpfn))
+    tabpfn.fit(X_train[:99], y_train[:99])
+    print("predicting reg")
+    print(tabpfn.predict(X_test, output_type="mean"))
 
-        # print(UserDataClient.get_data_summary())
-        # test predict_full
-        print("predicting ")
-        print(
-            tabpfn.predict(X_test[:30], output_type="full", quantiles=[0.1, 0.5, 0.9])
-        )
+    # print(UserDataClient.get_data_summary())
+    # test predict_full
+    print("predicting ")
+    print(
+        tabpfn.predict(X_test[:30], output_type="full", quantiles=[0.1, 0.5, 0.9])
+    )
