23d226a7bbfdb0c67c112bc3d97c8663c0a8c511
klemens-floege <klemens@priorlabs.ai>
2026-03-23 10:27:04 +0100

    linting

--- src/tabpfn/preprocessing/steps/encode_categorical_features_step.py
+++ src/tabpfn/preprocessing/steps/encode_categorical_features_step.py
@@ -30,7 +30,9 @@ def _get_all_cat_indices_after_onehot(
     that were passed through in the remainder.
     """
     onehot_input_cols = set(ct.transformers_[0][2])
-    onehot_out = list(range(n_output_features))[ct.output_indices_[ONE_HOT_ENCODER_NAME]]
+    onehot_out = list(range(n_output_features))[
+        ct.output_indices_[ONE_HOT_ENCODER_NAME]
+    ]
     # Find skipped categoricals in the remainder output
     remainder_start = ct.output_indices_["remainder"].start
     remainder_input_cols = sorted(set(range(ct.n_features_in_)) - onehot_input_cols)
