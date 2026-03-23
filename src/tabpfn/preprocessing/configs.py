5b6824b8868a88de6abbc3a160ea769a98a2d6fd
klemens-floege <klemens@priorlabs.ai>
2026-03-23 12:11:17 +0100

    add max_onehot_cardinality to docstrings

--- src/tabpfn/preprocessing/configs.py
+++ src/tabpfn/preprocessing/configs.py
@@ -35,6 +35,9 @@ class PreprocessorConfig:
             each estimator independently. If append to original is set to True we can
             still have more features.
         global_transformer_name: Name of the global transformer to use.
+        max_onehot_cardinality: Maximum number of unique values a categorical feature
+            can have to be one-hot encoded. Features with higher cardinality are passed
+            through unchanged. If None, all categorical features are one-hot encoded.
     """
 
     name: Literal[
