ffe7f4af75b76451e0bb90b7cacaa56acec580d0
Benjamin Jaeger <benjamin@priorlabs.ai>
2026-03-24 18:13:36 +0100

    Revert "Make the 2.6 model the default model (#835)"
    This reverts commit fb6f965f22554275f13336e1cd9c82eb9ffd46bb.

--- src/tabpfn/preprocessing/__init__.py
+++ src/tabpfn/preprocessing/__init__.py
@@ -13,6 +13,8 @@ from .ensemble import (
 )
 from .pipeline_interface import PreprocessingPipeline
 from .presets import (
+    default_classifier_preprocessor_configs,
+    default_regressor_preprocessor_configs,
     v2_5_classifier_preprocessor_configs,
     v2_5_regressor_preprocessor_configs,
     v2_6_classifier_preprocessor_configs,
@@ -29,6 +31,8 @@ __all__ = [
     "PreprocessorConfig",
     "RegressorEnsembleConfig",
     "clean_data",
+    "default_classifier_preprocessor_configs",
+    "default_regressor_preprocessor_configs",
     "fit_preprocessing",
     "generate_classification_ensemble_configs",
     "generate_regression_ensemble_configs",
