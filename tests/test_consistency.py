889944b7969539f4d53e0eb20ee0da6ca9d3ae09
Benjamin Jaeger <jaeger.benjamin7@gmail.com>
2026-03-27 15:54:45 +0100

    only fit with cache for 2.5

--- tests/test_consistency.py
+++ tests/test_consistency.py
@@ -122,6 +122,19 @@ TEST_CASES = {
         if version == ModelVersion.V2_6 or fit_mode == "fit_preprocessors"
     },
     **{
+        f"classifier_tiny_dataset_{version.value}_{fit_mode}": _ConsistencyCase(
+            data=_get_tiny_classification_data,
+            model=partial(
+                TabPFNClassifier.create_default_for_version,
+                fit_mode=fit_mode,
+                version=version,
+                **DEFAULT_CONFIG,
+            ),
+        )
+        for fit_mode in ["fit_with_cache"]
+        for version in [ModelVersion.V2_5]
+    },
+    **{
         f"regressor_tiny_dataset_{version.value}_{fit_mode}": _ConsistencyCase(
             data=_get_tiny_regression_data,
             model=partial(
@@ -135,6 +148,19 @@ TEST_CASES = {
         for version in [ModelVersion.V2_5, ModelVersion.V2_6]
         # Save compute by only running all the tests for the latest model.
         if version == ModelVersion.V2_6 or fit_mode == "fit_preprocessors"
+    },
+    **{
+        f"regressor_tiny_dataset_{version.value}_{fit_mode}": _ConsistencyCase(
+            data=_get_tiny_regression_data,
+            model=partial(
+                TabPFNRegressor.create_default_for_version,
+                fit_mode=fit_mode,
+                version=version,
+                **DEFAULT_CONFIG,
+            ),
+        )
+        for fit_mode in ["fit_with_cache"]
+        for version in [ModelVersion.V2_5]
     },
     "classifier_tiny_dataset_differentiable_input": _ConsistencyCase(
         data=lambda: _to_tensors(_get_tiny_classification_data()),
