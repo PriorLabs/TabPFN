b1fed6ee75dcfbbe4812056dad66182ea8d49c03
ANIRUDDHA ADAK <aniruddhaadak80@gmail.com>
2026-09-25 16:18:33 +0530

    style: format classifier differentiable fix

--- src/tabpfn/classifier.py
+++ src/tabpfn/classifier.py
@@ -728,7 +728,7 @@ class TabPFNClassifier(ClassifierMixin, BaseEstimator):
         y: torch.Tensor,
         rng: np.random.Generator,
     ) -> tuple[list[ClassifierEnsembleConfig], torch.Tensor, torch.Tensor]:
-        """Initialize the model for differentiable input.\n\n        Polynomial expansion is disabled because it is implemented with\n        non-differentiable NumPy/scikit-learn preprocessing.\n        """
+        """Initialize the model for differentiable input.\n\n        Polynomial expansion is disabled because it is implemented with\n        non-differentiable NumPy/scikit-learn preprocessing.\n"""
         validate_dataset_size(
             X=X,
             y=y,
